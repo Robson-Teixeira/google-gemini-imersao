c2e62df0d970006f2850d663930db49427c13f59
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-09-05 23:53:55 -0300

    Aula 03: Criando a base de dados com JavaScript

---
+++ src/scripts/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Chevrolet - Opala",
+        descricao: "O Opala, um ícone da indústria automobilística brasileira, marcou época entre as décadas de 60 e 90. Produzido pela Chevrolet, o modelo conquistou admiradores por seu design imponente, inspirado nos carros americanos da época, e por sua robustez mecânica. Disponível em diversas versões, desde a básica até a esportiva SS, o Opala atendia a diferentes públicos, tornando-se um verdadeiro símbolo de status e paixão nacional.",
+        linkVideo: "https://www.youtube.com/watch?v=DybtTEtPRr4",
+        linkSaibaMais: "https://pt.wikipedia.org/wiki/Chevrolet_Opala"
+    }, 
+    {
+        titulo: "Dodge - Charger",
+        descricao: "O Charger, um ícone da esportividade no Brasil, marcou época nos anos 70 com seu visual agressivo e motorização potente. Importado da matriz americana, o modelo rapidamente conquistou os brasileiros apaixonados por carros de alta performance, tornando-se um símbolo de status e velocidade. Apesar de sua curta trajetória no país, o Charger deixou um legado de admiração e saudosismo entre os entusiastas por muscle cars. ",
+        linkVideo: "https://www.youtube.com/watch?v=SvEPFNgMy6c",
+        linkSaibaMais: "https://pt.wikipedia.org/wiki/Dodge_Charger"
+    }, 
+    {
+        titulo: "Ford - Maverick",
+        descricao: "O Maverick, lançado no Brasil em 1973, rapidamente se tornou um sucesso de vendas. Com visual esportivo inspirado nos muscle cars americanos, o modelo oferecia opções de motores potentes e um estilo único. Apesar de sua vida curta no mercado brasileiro, o Maverick deixou sua marca na história como um ícone da esportividade e do design arrojado.",
+        linkVideo: "https://www.youtube.com/watch?v=OmmsP1UxQ6o",
+        linkSaibaMais: "https://pt.wikipedia.org/wiki/Ford_Maverick"
+    },     
+];
