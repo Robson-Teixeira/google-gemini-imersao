22c5f47432cec851cb14a00a187530a8a313cc49
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-09-06 22:43:54 -0300

    Aula 04: Implementando a funcionalidade de busca

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,29 @@
-console.log(dados);
+// Essa função é responsável por buscar os dados dos veículos e exibir os resultados na página.
+function pesquisarVeiculo() {
+
+    // Obtém a seção HTML onde os resultados serão inseridos.
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar o HTML dos resultados.
+    let resultadosPesquisaHtml = "";
+
+    // Itera sobre cada dado de veículo no array 'dados'.
+    for (let dado of dados) {
+
+        // Cria um novo elemento HTML para cada resultado.
+        resultadosPesquisaHtml += `
+        <div class="item-resultado">
+          <h2>
+            <a href="${dado.linkVideo}" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">
+            ${dado.descricao}
+          </p>
+          <a href="${dado.linkSaibaMais}" target="_blank">Saiba mais</a>
+          </div>
+      `;
+    }
+
+    // Insere o HTML gerado na seção de resultados.
+    section.innerHTML = resultadosPesquisaHtml;
+}
